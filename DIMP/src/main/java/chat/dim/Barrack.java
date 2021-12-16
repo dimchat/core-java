79196661b6f95c12868033e565188700c7dbc43b
Moky <albert.moky@gmail.com>
2021-12-17 01:22:58 +0800

    base classes Barrack, Transceiver

--- DIMP/src/main/java/chat/dim/Barrack.java
+++ DIMP/src/main/java/chat/dim/Barrack.java
@@ -30,10 +30,269 @@
  */
 package chat.dim;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chat.dim.crypto.EncryptKey;
+import chat.dim.crypto.VerifyKey;
+import chat.dim.protocol.Bulletin;
+import chat.dim.protocol.Document;
+import chat.dim.protocol.ID;
+import chat.dim.protocol.Meta;
+import chat.dim.protocol.NetworkType;
+import chat.dim.protocol.Visa;
+
 /**
  *  Entity Database
  *  ~~~~~~~~~~~~~~~
  *  Manage meta for all entities
  */
-public interface Barrack extends Entity.Delegate, User.DataSource, Group.DataSource {
+public abstract class Barrack implements Entity.Delegate, User.DataSource, Group.DataSource {
+
+    /**
+     *  Get all local users (for decrypting received message)
+     *
+     * @return users with private key
+     */
+    public abstract List<User> getLocalUsers();
+
+    /**
+     *  Get current user (for signing and sending message)
+     *
+     * @return User
+     */
+    public User getCurrentUser() {
+        List<User> users = getLocalUsers();
+        if (users == null || users.size() == 0) {
+            return null;
+        }
+        return users.get(0);
+    }
+
+    //-------- Entity Delegate
+
+    @Override
+    public User selectLocalUser(ID receiver) {
+        List<User> users = getLocalUsers();
+        if (users == null || users.size() == 0) {
+            throw new NullPointerException("local users should not be empty");
+        } else if (receiver.isBroadcast()) {
+            // broadcast message can decrypt by anyone, so just return current user
+            return users.get(0);
+        }
+        if (receiver.isGroup()) {
+            // group message (recipient not designated)
+            List<ID> members = getMembers(receiver);
+            if (members == null || members.size() == 0) {
+                // TODO: group not ready, waiting for group info
+                return null;
+            }
+            for (User item : users) {
+                if (members.contains(item.identifier)) {
+                    // DISCUSS: set this item to be current user?
+                    return item;
+                }
+            }
+        } else {
+            // 1. personal message
+            // 2. split group message
+            for (User item : users) {
+                if (receiver.equals(item.identifier)) {
+                    // DISCUSS: set this item to be current user?
+                    return item;
+                }
+            }
+        }
+        return null;
+    }
+
+    //-------- User DataSource
+
+    private EncryptKey getVisaKey(ID user) {
+        Document doc = getDocument(user, Document.VISA);
+        if (doc instanceof Visa) {
+            Visa visa = (Visa) doc;
+            if (visa.isValid()) {
+                return visa.getKey();
+            }
+        }
+        return null;
+    }
+    private VerifyKey getMetaKey(ID user) {
+        Meta meta = getMeta(user);
+        if (meta == null) {
+            //throw new NullPointerException("failed to get meta for ID: " + user);
+            return null;
+        }
+        return meta.getKey();
+    }
+
+    @Override
+    public EncryptKey getPublicKeyForEncryption(ID user) {
+        // 1. get key from visa
+        EncryptKey visaKey = getVisaKey(user);
+        if (visaKey != null) {
+            // if visa.key exists, use it for encryption
+            return visaKey;
+        }
+        // 2. get key from meta
+        VerifyKey metaKey = getMetaKey(user);
+        if (metaKey instanceof EncryptKey) {
+            // if visa.key not exists and meta.key is encrypt key,
+            // use it for encryption
+            return (EncryptKey) metaKey;
+        }
+        //throw new NullPointerException("failed to get encrypt key for user: " + user);
+        return null;
+    }
+
+    @Override
+    public List<VerifyKey> getPublicKeysForVerification(ID user) {
+        List<VerifyKey> keys = new ArrayList<>();
+        // 1. get key from visa
+        EncryptKey visaKey = getVisaKey(user);
+        if (visaKey instanceof VerifyKey) {
+            // the sender may use communication key to sign message.data,
+            // so try to verify it with visa.key here
+            keys.add((VerifyKey) visaKey);
+        }
+        // 2. get key from meta
+        VerifyKey metaKey = getMetaKey(user);
+        if (metaKey != null) {
+            // the sender may use identity key to sign message.data,
+            // try to verify it with meta.key
+            keys.add(metaKey);
+        }
+        assert keys.size() > 0 : "failed to get verify key for user: " + user;
+        return keys;
+    }
+
+    //-------- Group DataSource
+
+    private String getGroupSeed(ID group) {
+        String name = group.getName();
+        if (name != null) {
+            int len = name.length();
+            if (len == 0 || (len == 8 && name.equalsIgnoreCase("everyone"))) {
+                name = null;
+            }
+        }
+        return name;
+    }
+
+    protected ID getBroadcastFounder(ID group) {
+        String name = getGroupSeed(group);
+        if (name == null) {
+            // Consensus: the founder of group 'everyone@everywhere'
+            //            'Albert Moky'
+            return ID.FOUNDER;
+        } else {
+            // DISCUSS: who should be the founder of group 'xxx@everywhere'?
+            //          'anyone@anywhere', or 'xxx.founder@anywhere'
+            return ID.parse(name + ".founder@anywhere");
+        }
+    }
+    protected ID getBroadcastOwner(ID group) {
+        String name = getGroupSeed(group);
+        if (name == null) {
+            // Consensus: the owner of group 'everyone@everywhere'
+            //            'anyone@anywhere'
+            return ID.ANYONE;
+        } else {
+            // DISCUSS: who should be the owner of group 'xxx@everywhere'?
+            //          'anyone@anywhere', or 'xxx.owner@anywhere'
+            return ID.parse(name + ".owner@anywhere");
+        }
+    }
+    protected List<ID> getBroadcastMembers(ID group) {
+        List<ID> members = new ArrayList<>();
+        String name = getGroupSeed(group);
+        if (name == null) {
+            // Consensus: the member of group 'everyone@everywhere'
+            //            'anyone@anywhere'
+            members.add(ID.ANYONE);
+        } else {
+            // DISCUSS: who should be the member of group 'xxx@everywhere'?
+            //          'anyone@anywhere', or 'xxx.member@anywhere'
+            ID owner = ID.parse(name + ".owner@anywhere");
+            ID member = ID.parse(name + ".member@anywhere");
+            members.add(owner);
+            members.add(member);
+        }
+        return members;
+    }
+
+    @Override
+    public ID getFounder(ID group) {
+        // check broadcast group
+        if (group.isBroadcast()) {
+            // founder of broadcast group
+            return getBroadcastFounder(group);
+        }
+        // check group meta
+        Meta gMeta = getMeta(group);
+        if (gMeta == null) {
+            // FIXME: when group profile was arrived but the meta still on the way,
+            //        here will cause founder not found
+            return null;
+        }
+        // check each member's public key with group meta
+        List<ID> members = getMembers(group);
+        if (members != null) {
+            Meta mMeta;
+            for (ID item : members) {
+                mMeta = getMeta(item);
+                if (mMeta == null) {
+                    // failed to get member's meta
+                    continue;
+                }
+                if (gMeta.matches(mMeta.getKey())) {
+                    // if the member's public key matches with the group's meta,
+                    // it means this meta was generated by the member's private key
+                    return item;
+                }
+            }
+        }
+        // TODO: load founder from database
+        return null;
+    }
+
+    @Override
+    public ID getOwner(ID group) {
+        // check broadcast group
+        if (group.isBroadcast()) {
+            // owner of broadcast group
+            return getBroadcastOwner(group);
+        }
+        // check group type
+        if (NetworkType.POLYLOGUE.equals(group.getType())) {
+            // Polylogue's owner is its founder
+            return getFounder(group);
+        }
+        // TODO: load owner from database
+        return null;
+    }
+
+    @Override
+    public List<ID> getMembers(ID group) {
+        // check broadcast group
+        if (group.isBroadcast()) {
+            // members of broadcast group
+            return getBroadcastMembers(group);
+        }
+        // TODO: load members from database
+        return null;
+    }
+
+    @Override
+    public List<ID> getAssistants(ID group) {
+        Document doc = getDocument(group, Document.BULLETIN);
+        if (doc instanceof Bulletin) {
+            if (doc.isValid()) {
+                return ((Bulletin) doc).getAssistants();
+            }
+        }
+        // TODO: get group bots from SP configuration
+        return null;
+    }
 }
--- DIMP/src/main/java/chat/dim/Transceiver.java
+++ DIMP/src/main/java/chat/dim/Transceiver.java
@@ -30,12 +30,163 @@
  */
 package chat.dim;
 
+import java.util.Map;
+
+import chat.dim.crypto.SymmetricKey;
+import chat.dim.format.Base64;
+import chat.dim.format.JSON;
+import chat.dim.format.UTF8;
+import chat.dim.protocol.Content;
+import chat.dim.protocol.ID;
 import chat.dim.protocol.InstantMessage;
+import chat.dim.protocol.Message;
 import chat.dim.protocol.ReliableMessage;
+import chat.dim.protocol.SecureMessage;
 
 /**
  *  Message Transceiver
  *  ~~~~~~~~~~~~~~~~~~~
  */
-public interface Transceiver extends CipherKeyDelegate, InstantMessage.Delegate, ReliableMessage.Delegate {
+public abstract class Transceiver implements InstantMessage.Delegate, ReliableMessage.Delegate {
+
+    protected abstract Entity.Delegate getEntityDelegate();
+
+    protected boolean isBroadcast(Message msg) {
+        ID receiver = msg.getGroup();
+        if (receiver == null) {
+            receiver = msg.getReceiver();
+        }
+        return receiver.isBroadcast();
+    }
+
+    //-------- InstantMessageDelegate
+
+    @Override
+    public byte[] serializeContent(Content content, SymmetricKey password, InstantMessage iMsg) {
+        // NOTICE: check attachment for File/Image/Audio/Video message content
+        //         before serialize content, this job should be do in subclass
+        return JSON.encode(content);
+    }
+
+    @Override
+    public byte[] encryptContent(byte[] data, SymmetricKey password, InstantMessage iMsg) {
+        return password.encrypt(data);
+    }
+
+    @Override
+    public Object encodeData(byte[] data, InstantMessage iMsg) {
+        if (isBroadcast(iMsg)) {
+            // broadcast message content will not be encrypted (just encoded to JsON),
+            // so no need to encode to Base64 here
+            return UTF8.decode(data);
+        }
+        return Base64.encode(data);
+    }
+
+    @Override
+    public byte[] serializeKey(SymmetricKey password, InstantMessage iMsg) {
+        if (isBroadcast(iMsg)) {
+            // broadcast message has no key
+            return null;
+        }
+        return JSON.encode(password);
+    }
+
+    @Override
+    public byte[] encryptKey(byte[] data, ID receiver, InstantMessage iMsg) {
+        assert !isBroadcast(iMsg) : "broadcast message has no key: " + iMsg;
+        // NOTICE: make sure the receiver's public key exists
+        User user = getEntityDelegate().getUser(receiver);
+        return user.encrypt(data);
+    }
+
+    @Override
+    public Object encodeKey(byte[] key, InstantMessage iMsg) {
+        assert !isBroadcast(iMsg) : "broadcast message has no key: " + iMsg;
+        return Base64.encode(key);
+    }
+
+    //-------- SecureMessageDelegate
+
+    @Override
+    public byte[] decodeKey(Object key, SecureMessage sMsg) {
+        assert !isBroadcast(sMsg) : "broadcast message has no key: " + sMsg;
+        return Base64.decode((String) key);
+    }
+
+    @Override
+    public byte[] decryptKey(byte[] key, ID sender, ID receiver, SecureMessage sMsg) {
+        // NOTICE: the receiver will be group ID in a group message here
+        assert !isBroadcast(sMsg) : "broadcast message has no key: " + sMsg;
+        // decrypt key data with the receiver/group member's private key
+        ID identifier = sMsg.getReceiver();
+        User user = getEntityDelegate().getUser(identifier);
+        return user.decrypt(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public SymmetricKey deserializeKey(byte[] key, ID sender, ID receiver, SecureMessage sMsg) {
+        // NOTICE: the receiver will be group ID in a group message here
+        assert !isBroadcast(sMsg) : "broadcast message has no key: " + sMsg;
+        Map<String, Object> dict = (Map<String, Object>) JSON.decode(key);
+        // TODO: translate short keys
+        //       'A' -> 'algorithm'
+        //       'D' -> 'data'
+        //       'V' -> 'iv'
+        //       'M' -> 'mode'
+        //       'P' -> 'padding'
+        return SymmetricKey.parse(dict);
+    }
+
+    @Override
+    public byte[] decodeData(Object data, SecureMessage sMsg) {
+        if (isBroadcast(sMsg)) {
+            // broadcast message content will not be encrypted (just encoded to JsON),
+            // so return the string data directly
+            return UTF8.encode((String) data);
+        }
+        return Base64.decode((String) data);
+    }
+
+    @Override
+    public byte[] decryptContent(byte[] data, SymmetricKey password, SecureMessage sMsg) {
+        return password.decrypt(data);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Content deserializeContent(byte[] data, SymmetricKey password, SecureMessage sMsg) {
+        assert sMsg.getData() != null : "message data empty";
+        Map<String, Object> dict = (Map<String, Object>) JSON.decode(data);
+        // TODO: translate short keys
+        //       'T' -> 'type'
+        //       'N' -> 'sn'
+        //       'G' -> 'group'
+        return Content.parse(dict);
+    }
+
+    @Override
+    public byte[] signData(byte[] data, ID sender, SecureMessage sMsg) {
+        User user = getEntityDelegate().getUser(sender);
+        return user.sign(data);
+    }
+
+    @Override
+    public Object encodeSignature(byte[] signature, SecureMessage sMsg) {
+        return Base64.encode(signature);
+    }
+
+    //-------- ReliableMessageDelegate
+
+    @Override
+    public byte[] decodeSignature(Object signature, ReliableMessage rMsg) {
+        return Base64.decode((String) signature);
+    }
+
+    @Override
+    public boolean verifyDataSignature(byte[] data, byte[] signature, ID sender, ReliableMessage rMsg) {
+        User user = getEntityDelegate().getUser(sender);
+        return user.verify(data, signature);
+    }
 }
